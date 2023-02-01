ae5b388f8be6f27486b603bd0054ce00e1052ada
gitxprashant <dev.prashantpathak@gmail.com>
2023-02-01 15:15:30 +0530

    player2game

---
+++ game.js
@@ -0,0 +1,80 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS ='circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+]
+
+const restartButton = document.getElementById('button')
+const winningMessageElement = document.getElementById('winningMessages')
+const winningMessage =document.querySelector('[messages-text]')
+const cellElement = document.querySelectorAll('[data-cell]');
+let circleTurn
+
+startGame()
+restartButton.addEventListener('click', startGame)
+
+function startGame(){
+  circleTurn = false
+  cellElement.forEach(cell => {
+    cell.classList.remove(X_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, {once : true})
+  })
+  winningMessageElement.classList.remove('show')
+} 
+
+function handleClick(e){
+  
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+  placeMark(cell, currentClass)
+  if(checkWin(currentClass)){
+    endGame(false)
+  } else if(isDraw()){
+    endGame(true)
+  } else{
+    switchTurns()
+  }
+}
+
+
+function placeMark(cell, currentClass){
+  cell.classList.add(currentClass)
+}
+
+function switchTurns(){
+  circleTurn = !circleTurn
+}
+
+function checkWin(currentClass){
+  return WINNING_COMBINATIONS.some(combinations => {
+    return combinations.every(index => {
+      return cellElement[index].classList.contains(currentClass)
+    })
+  })
+}
+
+function endGame(draw){
+  if(draw){
+    winningMessage.innerText = "Draw!"
+  }else{
+    winningMessage.innerText = `${circleTurn ? "O's" : "X's"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+function isDraw(){
+  return [...cellElement].every(cell => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS) 
+  })
+}
+
+
